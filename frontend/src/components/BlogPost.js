72f5abe77ca9daa89f7773a1f06d59de396755da
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-02-04 15:59:51 -0300

    refactoring

--- frontend/src/components/BlogPost.js
+++ frontend/src/components/BlogPost.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-import axios from "axios";
+import instance from "../util/axios";
 
 const BlogPost = () => {
   const [post, setPost] = useState({});
@@ -9,15 +8,26 @@ const BlogPost = () => {
   let { slug } = useParams();
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(`http://127.0.0.1:8000/api/blog/${slug}`);
+      const request = await instance.get(`api/blog/${slug}`);
       console.log(request);
       setPost(request.data);
       return request;
     }
     fetchData();
-  }, []);
+  }, [slug]);
+
   return (
     <>
+      <h1>{post.title}</h1>
+      <h5 className="font-italic">{post.excerpt}</h5>
+      <p className="small">{`${new Date(post.date_published).toLocaleString(
+        "es-CL",
+        {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+        }
+      )}`}</p>
       <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </>
   );
